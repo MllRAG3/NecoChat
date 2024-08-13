34dabf87ca2313b6e291ec4d29aaf932a9bc5fbd
v <t.shagin@gmail.com>
2024-08-13 13:27:52 +0300

    feat: Добавлена ебучая визуализация

--- modules/processes/StatsProcess.py
+++ modules/processes/StatsProcess.py
@@ -1,9 +1,12 @@
+import os
+
 from modules.processes.BaseHandler import BaseHandler
 from pyrogram import handlers, filters, types
 from modules.database import GetOrCreate
 from modules.filters import chat_is_group_filter
 from modules.database.models import Messages
 
+from modules.util import make_plt_pic, get_all_logs_grouped_by_days, get_all_today_hours
 from datetime import datetime, timedelta
 
 
@@ -23,18 +26,27 @@ class SendUserStats(BaseHandler):
 
     async def func(self, _, message: types.Message):
         await GetOrCreate(message=message).log()
+        member = await GetOrCreate(message=message).chat_member()
         now: datetime = datetime.now()
-        per_day = len(Messages.select().where(Messages.updated_at.between(now - timedelta(seconds=10), now)))
-        per_week = len(Messages.select().where(Messages.updated_at.between(now - timedelta(seconds=30), now)))
-        per_month = len(Messages.select().where(Messages.updated_at.between(now - timedelta(seconds=60), now)))
+        per_day = len(Messages.select().where(
+            Messages.updated_at.between(now - timedelta(days=1), now) & Messages.sender == member))
+        per_week = len(Messages.select().where(
+            Messages.updated_at.between(now - timedelta(days=7), now) & Messages.sender == member))
+        per_month = len(Messages.select().where(
+            Messages.updated_at.between(now - timedelta(days=30), now) & Messages.sender == member))
         per_all = len(Messages.select())
 
-        member = await GetOrCreate(message=message).chat_member()
-        await message.reply(
-            f"Сообщения пользователя {member.config[0].custom_name}"
-            f"\nДень | Неделя | Месяц | Все время"
-            f"\n{per_day} | {per_week} | {per_month} | {per_all}"
+        await message.reply_photo(
+            photo=open(make_plt_pic(
+                cust_num=-1,
+                today=get_all_today_hours(member),
+                all_time=get_all_logs_grouped_by_days(member),
+            ), "rb"),
+            caption=f"Сообщения пользователя {member.config[0].custom_name}"
+                    f"\nДень | Неделя | Месяц | Все время"
+                    f"\n{per_day} | {per_week} | {per_month} | {per_all}"
         )
+        os.remove("-1.png")
 
 
 class SendChatStats(BaseHandler):  # beta
--- modules/util/util.py
+++ modules/util/util.py
@@ -1,9 +1,9 @@
-import matplotlib.pyplot as plt
-from modules.database.models import models
-from modules.database import db
-
-from datetime import datetime
 import re
+import matplotlib.pyplot as plt
+from datetime import datetime, timedelta
+
+from modules.database.models import models, Messages, ChatMembers
+from modules.database import db
 
 
 def create_tables() -> None:
@@ -29,34 +29,86 @@ def safe_to_datetime(value: str, f: str = "%d/%m/%Y %H:%M") -> datetime | None:
         return None
 
 
+def compress_list(lst: list, to: int, i=0):
+    if len(lst) <= to: return lst
+    if i + 2 > len(lst): i = 0
+    if isinstance(lst[i], int):
+        lst[i] = (lst.pop(i + 1) + lst[i]) // 2
+    else:
+        del lst[i + 1]
+
+    compress_list(lst=lst, to=to, i=i + 1)
+    return lst
+
+
 def make_plt_pic(
         cust_num: int,
         today: list[int],
         all_time: list[int],
-        usernik: str = "Хз кто ето"
-) -> None:
-    plt.figure(figsize=(10, 5))
+) -> str:
+    plt.figure(figsize=(23, 20))
     plt.style.use("grayscale")
 
     plt.subplot(211)  # rows / cols / position
-    plt.title("сегодня")
-    plt.ylabel("Сообщения")
+    plt.title("СЕГОДНЯ")
+    plt.ylabel("СООБЩЕНИЯ")
+    plt.xlabel("ЧАС")
+    plt.grid()
     plt.minorticks_on()
+    print(today)
     plt.bar(
         [i for i in range(1, len(today) + 1)],
         today
     )
 
     plt.subplot(212)
-    plt.title("за все время")
-    plt.xlabel("День")
-    plt.ylabel("Сообщения")
+    plt.title("ЗА ВСЕ ВРЕМЯ")
+    plt.xlabel("ДЕНЬ")
+    plt.ylabel("СООБЩЕНИЯ")
+    plt.grid()
     plt.minorticks_on()
 
+    all_time = compress_list(all_time, to=100)
+    print(all_time)
     plt.bar(
         [i for i in range(len(all_time))],
         all_time
     )
 
-    plt.suptitle(f'С Т А Т А   П О Л Ь З О В А Т Е Л Я   {usernik}')
+    plt.suptitle('С Т А Т А   П О Л Ь З О В А Т Е Л Я')
     plt.savefig(str(cust_num))
+    return str(cust_num) + ".png"
+
+
+def get_all_logs_grouped_by_days(member: ChatMembers) -> list[int]:
+    all_logs = Messages \
+        .select() \
+        .where(Messages.sender == member) \
+        .order_by(Messages.updated_at)
+
+    grouped = []
+    cur_date = all_logs[0].updated_at  # here we start
+    for _ in range((all_logs[-1].updated_at - cur_date).days + 1):
+        grouped.append(
+            len(tuple(filter(lambda x: cur_date <= x.updated_at <= (cur_date + timedelta(days=1)), all_logs))))
+        cur_date += timedelta(days=1)
+
+    return grouped
+
+
+def get_all_today_hours(member: ChatMembers) -> list[int]:
+    now = datetime.now()
+    all_today_logs = Messages \
+        .select() \
+        .where(
+            (Messages.sender == member) & (Messages.updated_at.between(now - timedelta(hours=now.hour, minutes=now.minute, seconds=now.second, microseconds=now.microsecond), now))
+        ) \
+        .order_by(Messages.updated_at)
+
+    grouped = []
+    c = datetime(year=now.year, month=now.month, day=now.day, hour=0, minute=0, second=0, microsecond=0)
+    for _ in range(1, 25):
+        grouped.append(len(tuple(filter(lambda x: c <= x.updated_at <= (c + timedelta(hours=1)), all_today_logs))))
+        c += timedelta(hours=1)
+
+    return grouped
